5c6f225cfa74b591c589c80702d6875a9d195a60
WenzhuoXu <wxu2@andrew.cmu.edu>
2023-03-08 15:40:42 -0500

    Construct model based on paper given

--- graph_model.py
+++ graph_model.py
@@ -3,32 +3,33 @@ import torch_geometric.nn as pyg_nn
 
 
 class GraphNet(torch.nn.Module):
-    def __init__(self, input_dim, hidden_dim, output_dim, num_layers, dropout):
+    # Graph Neural Network.
+    # Network processes nodal and elemental data separatly with linear layers before concatenating them.
+    # The network then processes the concatenated data with a series of graph convolutional layers.
+    def __init__(self, input_dim_node, input_dim_element, hidden_dim, output_dim, num_layers, dropout):
         super(GraphNet, self).__init__()
-        self.input_dim = input_dim
-        self.hidden_dim = hidden_dim
-        self.output_dim = output_dim
         self.num_layers = num_layers
         self.dropout = dropout
-
+        self.node_feature_process = torch.nn.Linear(input_dim_node, hidden_dim)
+        self.element_feature_process = torch.nn.Linear(input_dim_element, hidden_dim)
         self.convs = torch.nn.ModuleList()
-        self.convs.append(pyg_nn.GCNConv(input_dim, hidden_dim))
-        for i in range(num_layers - 2):
-            self.convs.append(pyg_nn.GCNConv(hidden_dim, hidden_dim))
-        self.convs.append(pyg_nn.GCNConv(hidden_dim, output_dim))
+        for i in range(num_layers):
+            conv = pyg_nn.GENConv(hidden_dim, hidden_dim)
+            norm = pyg_nn.LayerNorm(hidden_dim)
+            act = torch.nn.LeakyReLU()
 
-        self.bns = torch.nn.ModuleList()
-        for i in range(num_layers - 1):
-            self.bns.append(torch.nn.BatchNorm1d(hidden_dim))
+            layer = pyg_nn.DeepGCNLayer(conv, norm, act, dropout)
+            self.convs.append(layer)
 
-        self.dropout = dropout
+        self.output_layer = pyg_nn.GENConv(hidden_dim, output_dim)
 
     def forward(self, data):
-        x, edge_index = data.x, data.edge_index
-        for i in range(self.num_layers - 1):
-            x = self.convs[i](x, edge_index)
-            x = self.bns[i](x)
-            x = torch.relu(x)
-            x = torch.nn.functional.dropout(x, p=self.dropout, training=self.training)
-        x = self.convs[-1](x, edge_index)
-        return torch.sigmoid(x)
+        x, edge_index, pos = data.x, data.edge_index, data.pos
+        x_node = self.node_feature_process(x)
+        x_element = self.element_feature_process(data.pos)
+        x = torch.add(x_node, x_element)
+        for i in range(self.num_layers):
+            x = self.convs[i](x, edge_index, pos)
+        x = self.output_layer(x, edge_index, pos)
+        return x
+        
